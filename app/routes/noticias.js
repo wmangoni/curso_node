19401100cca91c0388f5408bf247e8cd04aa5583
williammangoni <willrpg@hotmail.com>
2017-02-14 13:42:42 -0200

    conexao com banco mysql

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,5 +1,14 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app) {
+
+	var connection = dbConnection();
+	
 	app.get('/noticias', function(req, res) {
-		res.render("noticias/noticias");
+
+		connection.query('select * from noticias', function(error, result) {
+			res.render("noticias/noticias", {noticias: result});
+		});
+
 	});
 }
